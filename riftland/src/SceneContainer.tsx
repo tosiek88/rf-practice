944731fcf1ae0c316d98af46714df94d14877adc
toch <tocha.mateusz@gmail.com>
2023-04-18 19:46:56 +0200

    fix: pulblic assets, and path

--- riftland/src/SceneContainer.tsx
+++ riftland/src/SceneContainer.tsx
@@ -2,11 +2,12 @@ import { Suspense } from 'react'
 import {Environment, OrbitControls, PerspectiveCamera } from '@react-three/drei'
 
 export const SceneContainer=()=>{
+  console.debug(import.meta.env)
   return <Suspense fallback={null}>
     <Environment background={'only'} files={'/textures/bg.hdr'}/>
     <Environment background={'only'} files={'/textures/envmap.hdr'}/>
 
-    <PerspectiveCamera makeDefault fov={50} position={[-1.75, 10.85, 20.35]} />
+    <PerspectiveCamera makeDefault fov={130} position={[-1.75, 10.85, 20.35]} />
     <OrbitControls target={[1,5,0]} maxPolarAngle={Math.PI*0.5} />
   </Suspense>
 }
--- riftland/vite.config.ts
+++ riftland/vite.config.ts
@@ -4,5 +4,10 @@ import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: './',
+  build: {
+    outDir: 'dist',
+  },
+  publicDir: 'public',
   plugins: [react(), eslint()],
 });

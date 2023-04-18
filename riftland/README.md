4fb034941729424740de0a505b3efa77035935e1
toch <tocha.mateusz@gmail.com>
2023-04-18 13:55:03 +0200

    feat: install eslint_d, and plugins

---
+++ riftland/README.md
@@ -0,0 +1,10 @@
+# Riftland
+
+### Eslint
+
+Make sure that `eslint_d` or `eslint` is installed globally.
+
+```bash
+npm install -g eslint_d
+```
+
--- riftland/src/App.tsx
+++ riftland/src/App.tsx
@@ -1,18 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  console.debug('test')
+
   return (
-    <div className="App">
+    <div className='App'>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
